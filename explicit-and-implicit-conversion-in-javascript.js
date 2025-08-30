c41cdb62cc5a38a6a5f90b5605709de7e8704b41
hiideehoo <hiideeho@gmail.com>
2025-08-30 16:36:20 -0500

    I think I'm getting the hang of this.

--- explicit-and-implicit-conversion-in-javascript.js
+++ explicit-and-implicit-conversion-in-javascript.js
@@ -20,13 +20,26 @@ Use console.log() to clearly show the before-and-after type conversions.
 
 
 let result = "5" - 2;
-console.log("The result is: " + result);
+console.log("The result is: " + result); // this is fine
 
 let isValid = Boolean("false");
 if (isValid) {
     console.log("This is valid!");
 }
 
-let age = "25";
-let totalAge = age + 5;
-console.log("Total Age: " + totalAge);
+/*
+I honestly can't tell if the expected output is true or false. 
+But I know since Boolean("false") is truthy, the command will run.
+I'm interpreting this as working properly.
+*/
+
+let age = Number("25"); //converted "25" by adding Number()
+let totalAge = age + 5; // the + otherwise makes this a string concatenation
+console.log("Total Age: " + totalAge); // now the output should be 30 instead of 255
+
+if (null) { // implicitly converted to boolean
+  console.log("This is falsy");
+}
+
+let eulerNumber = 2.71828;
+console.log("Euler's number rounded down: " + parseInt(eulerNumber)); // explicitly converted to an integer
